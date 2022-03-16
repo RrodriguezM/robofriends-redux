9bcca2f3785cc6edf8a460f16cca1654436d0211
Rafael <rafael.a.rodriguez.m@gmail.com>
2022-03-16 04:23:30 -0400

    Add service worker

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import App from './containers/App';
 import 'tachyons'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { searchRobots } from './reducers';
 
@@ -21,6 +22,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
